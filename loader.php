a063a7e002d159e4fe40c2791b3e89bd3fa7b461
michal <michal@Michal-PC>
2012-03-28 14:04:43 +0200

    Login Application

--- loader.php
+++ loader.php
@@ -7,4 +7,5 @@ include_once (__DIR__ . '/Couchover/Debugger.class.php');
 include_once (__DIR__ . '/Couchover/BaseController.class.php');
 include_once (__DIR__ . '/Couchover/BaseModel.class.php');
 include_once (__DIR__ . '/Couchover/Router.class.php');
+include_once (__DIR__ . '/Couchover/Login.class.php');
 include_once (__DIR__ . '/Couchover/Template.class.php');
