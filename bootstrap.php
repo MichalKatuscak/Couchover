505d77ea685cb20b6480d4b3ddd5c2a9262c023e
michal <michal@Michal-PC>
2012-03-28 14:09:40 +0200

    Add session_start()

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,9 @@ use Couchover\Application;
 use Couchover\Router;
 
 @header('X-Powered-By: Couchover Framework');
-@header('Content-Type: text/html; charset=utf-8'); 
+@header('Content-Type: text/html; charset=utf-8');
+
+session_start();
 
 include_once (__DIR__ . '/config.php');
 include_once (__DIR__ . '/loader.php');
