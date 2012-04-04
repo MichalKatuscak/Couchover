edadd2045cde868279817edbfc79bac68ca1ba4d
Michal Katuscak <katuscak@hotmail.cz>
2012-04-04 19:22:49 +0200

    Add Session support

--- loader.php
+++ loader.php
@@ -8,4 +8,5 @@ include_once (__DIR__ . '/Couchover/BaseController.class.php');
 include_once (__DIR__ . '/Couchover/BaseModel.class.php');
 include_once (__DIR__ . '/Couchover/Router.class.php');
 include_once (__DIR__ . '/Couchover/Login.class.php');
+include_once (__DIR__ . '/Couchover/Session.class.php');
 include_once (__DIR__ . '/Couchover/Template.class.php');
